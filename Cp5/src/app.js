042a09339a78c3ab44b6066b61e63092b5efb2b1
Tural Suleymani <suleymani.tural@bp.com>
2024-05-12 19:48:02 +0400

    added cors

--- Cp5/src/app.js
+++ Cp5/src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const v1 = require('./routes/v1');
+const cors = require('cors');
 
 const app = express();
 
+//added while implementing comunication with traqnsaction service, in Cp6
+const corsOptions = {
+    origin: 'http://localhost:3001',//(https://your-client-app.com)
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 // service
 app.use(express.json());
 
