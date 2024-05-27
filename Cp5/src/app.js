4c2b99f367783ae30e842f45d8f63237508110e0
Tural Suleymani <suleymani.tural@bp.com>
2024-05-27 13:47:42 +0400

    git ignore changed

--- Cp5/src/app.js
+++ Cp5/src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-//added while implementing comunication with traqnsaction service, in Cp6
+//added while implementing communication with transaction service, for Cp6
 const corsOptions = {
     origin: 'http://localhost:3001',//(https://your-client-app.com)
     optionsSuccessStatus: 200,
