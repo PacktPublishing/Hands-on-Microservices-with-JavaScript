293b4c36a1199f33bea2a0d6ed6ef795f2efef46
Tural Suleymani <suleymani.tural@bp.com>
2024-07-01 23:50:55 +0400

    removed unnecessary information from response

--- Cp9/authMicroservice/src/controllers/user.js
+++ Cp9/authMicroservice/src/controllers/user.js
@@ -12,7 +12,7 @@ const createUser = async (req, res) => {
             return res.status(400).json({ message: 'Email already exists' });
         }
         const user = await userService.createUser({ email, password });
-        res.status(201).json({ message: 'User created successfully', user: user });
+        res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server error' });
